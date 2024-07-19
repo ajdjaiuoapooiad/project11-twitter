bbd5d173bf4d00f25e2417c3c9994b892f0ed185
user <user@usernoMacBook-Air.local>
2024-07-19 12:06:35 +0900

    commit2

--- twitter/models.py
+++ twitter/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
     title=models.CharField('タイトル',max_length=20)
     text=models.TextField('本文',max_length=140)
-    thumbnail = models.ImageField('画像（空欄可）', upload_to='thumbnails/', null=True, blank=True)
+    thumbnail = models.ImageField('画像（空欄可）', upload_to='media/thumbnails/', null=True, blank=True)
     created_at=models.DateTimeField('日付',auto_now_add=True)
     update_at=models.DateTimeField('更新日',auto_now=True)
     
